d764d9b3a99c46132de5ca6666dd7c44fccb8a1c
Bobby Alestra <bobbyalestra135@gmail.com>
2021-11-01 17:01:21 -0400

    making space invaders, made a border for game

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import turtle
 
 # Set up the Screen
-wn = turtle.screen()
+wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Space Invaders")
 
@@ -12,3 +12,12 @@ wn.title("Space Invaders")
 border_pen = turtle.Turtle()
 border_pen.speed(0)
 border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
